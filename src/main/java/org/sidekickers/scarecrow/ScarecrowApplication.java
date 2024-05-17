d0e11a679c1fb4d878de700819f83483a384ea5e
mrbrique <locus.chan.dev@gmail.com>
2024-05-17 10:38:34 +0100

    commit project skeleton

---
+++ src/main/java/org/sidekickers/scarecrow/ScarecrowApplication.java
@@ -0,0 +1,13 @@
+package org.sidekickers.scarecrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScarecrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScarecrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sidekickers/scarecrow/ScarecrowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sidekickers.scarecrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarecrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
